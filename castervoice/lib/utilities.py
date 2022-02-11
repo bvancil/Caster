ff279c6c718a71305ec412ca93b1bbab4d67afb6
LexiconCode <CasterVoice@protonmail.com>
2022-02-10 19:05:28 -0600

    Clear hud when restarting Caster

--- castervoice/lib/utilities.py
+++ castervoice/lib/utilities.py
@@ -16,6 +16,7 @@ from urllib.parse import unquote
 import tomlkit
 from castervoice.lib.clipboard import Clipboard
 from castervoice.lib.util import guidance
+from castervoice.asynch import hud_support
 from dragonfly import Key, Window, get_current_engine
 
 try:  # Style C -- may be imported into Caster, or externally
